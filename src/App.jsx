00a7b9b9070b8bb5ee43fff48dee5d1d27bc9886
nick-leslie <nick.leslie303@gmail.com>
2024-01-15 17:46:10 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    //Add your name in a new div tag below
+    //Commit your changes and submit a pull request
+    return (
+      <div>
+          Nick Leslie
+      </div>
+    )
+}
+
+export default App
