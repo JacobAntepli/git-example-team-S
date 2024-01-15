fa6f5aee0c32f265f7d471c6514d10487a670107
nick-leslie <nick.leslie303@gmail.com>
2024-01-15 18:24:37 -0500

    added team name section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 
 function App() {
-    //Add your name in a new div tag below
+    //Add your name in a new h1 tag below
     //Commit your changes and submit a pull request
     return (
       <div>
-          ta name here
+          <h1>Team name</h1>
+          <h1> add your name here</h1>
       </div>
     )
 }
