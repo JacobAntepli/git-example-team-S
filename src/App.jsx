7c66d0f869b8b62f4c8ad6a8712fa09aa982d3db
Matt <mattc@mill5.com>
2024-03-19 19:45:23 -0400

    Added name Matthew Crane

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-    //Add your name in a new h1 tag below
-    //Commit your changes and submit a pull request
-    return (
-      <div>
-          <h1>Team name</h1>
-          <h1>Matthew Brown</h1>
-          <h1>Thomas Beattie</h1>
-          <h1> Jose Manuel</h1>
-          <h1>Dorothy Alexander</h1>
-          <h2>Jacob Antepli</h2>
-          <h1>Abelardo Broche Ortiz</h1>
-          <h1>Christian Consiglio</h1>
-      </div>
-    )
+  //Add your name in a new h1 tag below
+  //Commit your changes and submit a pull request
+  return (
+    <div>
+      <h1>Team name</h1>
+      <h1>Matthew Brown</h1>
+      <h1>Thomas Beattie</h1>
+      <h1> Jose Manuel</h1>
+      <h1>Dorothy Alexander</h1>
+      <h2>Jacob Antepli</h2>
+      <h1>Abelardo Broche Ortiz</h1>
+      <h1>Christian Consiglio</h1>
+      <h1>Matthew Crane</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
