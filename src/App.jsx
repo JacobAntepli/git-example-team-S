f03f9c24baa856146bee5f00085c8caec8d641f1
Jeffrey Li <jeffreyli@Jeffreys-MacBook-Pro.local>
2024-03-19 20:30:20 -0400

    added name

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ function App() {
     //Add your name in a new h1 tag below
     //Commit your changes and submit a pull request
     return (
-      <div>
-          <h1>Serpentine Serpentine</h1>
-          <h2>Jacob Antepli</h2>
-          <h1>Hanzalah Qamar</h1>
-          <h1>Christopher Hunt</h1>
-          <h1>Kimberly Cummings</h1>
-      </div>
+        <div>
+            <h1>Serpentine Serpentine</h1>
+            <h2>Jacob Antepli</h2>
+            <h1>Hanzalah Qamar</h1>
+            <h1>Christopher Hunt</h1>
+            <h1>Kimberly Cummings</h1>
+            <h1>jeffrey li</h1>
+        </div>
     )
 }
 
